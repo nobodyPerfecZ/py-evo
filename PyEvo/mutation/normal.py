ab604f81a4b4dd38e82850a830368701c482c6bb
Dennis Jabs <dennisjabs1@outlook.de>
2023-11-16 20:17:45 +0100

    D. Jabs: - Added unittests to LinearDecay, EpsilonDecay Gaussian Mutation

--- PyEvo/mutation/normal.py
+++ PyEvo/mutation/normal.py
@@ -61,7 +61,6 @@ class DecayGaussianMutation(GaussianMutation, ABC):
         super().__init__(mean, max_std, prob)
         self._min_std = min_std
         self._max_std = max_std
-        self._steps = steps
 
     @abstractmethod
     def _update(self):
@@ -108,7 +107,7 @@ class EpsilonDecayGaussianMutation(DecayGaussianMutation):
         assert steps > 0, f"Illegal steps {steps}. The argument should be higher than 0!"
         super().__init__(mean, min_std, max_std, prob)
         self._steps = steps
-        self.decay_factor = (self._min_std / self._max_std) ** (1 / self._steps)
+        self._decay_factor = (self._min_std / self._max_std) ** (1 / self._steps)
 
     def _update(self):
-        self._std = max(self._std * self.decay_factor, self._min_std)
+        self._std = max(self._std * self._decay_factor, self._min_std)
--- tests/mutation/test_normal.py
+++ tests/mutation/test_normal.py
@@ -5,7 +5,7 @@ from PyHyperparameterSpace.hp.constant import Constant
 from PyHyperparameterSpace.hp.continuous import Float
 from PyHyperparameterSpace.space import HyperparameterConfigurationSpace
 
-from PyEvo.mutation.normal import GaussianMutation
+from PyEvo.mutation.normal import GaussianMutation, LinearDecayGaussianMutation, EpsilonDecayGaussianMutation
 
 
 class TestGaussianMutation(unittest.TestCase):
@@ -42,6 +42,81 @@ class TestGaussianMutation(unittest.TestCase):
         """
         new_pop = self.mutator.mutate(self.random, self.cs, self.pop, self.fitness, self.optimizer_min)
 
+        self.assertEqual(len(self.pop), len(new_pop))
+        self.assertNotEqual(self.pop, new_pop)
+
+
+class TestLinearDecayGaussianMutation(unittest.TestCase):
+    """
+    Tests the class LinearDecayGaussianMutation.
+    """
+
+    def setUp(self):
+        self.random = np.random.RandomState(0)
+        self.cs = HyperparameterConfigurationSpace(
+            values={
+                "max_episodes": Constant("max_episodes", default=3),
+                "max_episode_length": Constant("max_episode_length", default=1000),
+                "hidden1_shape": Constant("hidden1_shape", default=64),
+                "hidden2_shape": Constant("hidden2_shape", default=32),
+                "fc1.weight": Float("fc1.weight", bounds=(-1.0, 1.0), shape=(64, 8)),
+                "fc1.bias": Float("fc1.bias", bounds=(-1.0, 1.0), shape=(64,)),
+                "fc2.weight": Float("fc2.weight", bounds=(-1.0, 1.0), shape=(32, 64)),
+                "fc2.bias": Float("fc2.bias", bounds=(-1.0, 1.0), shape=(32,)),
+                "fc3.weight": Float("fc3.weight", bounds=(-1.0, 1.0), shape=(4, 32)),
+                "fc3.bias": Float("fc3.bias", bounds=(-1.0, 1.0), shape=(4,)),
+            },
+            seed=0,
+        )
+        self.pop = self.cs.sample_configuration(10)
+        self.fitness = [-92, -17, 22, 56, -96, -20, 76, 29, -48, -56]
+        self.optimizer_min = "min"
+        self.optimizer_max = "max"
+        self.mutator = LinearDecayGaussianMutation(mean=0.0, max_std=1.0, min_std=0.1, prob=1.0, steps=9)
+
+    def test_mutate(self):
+        """
+        Tests the method mutate().
+        """
+        new_pop = self.mutator.mutate(self.random, self.cs, self.pop, self.fitness, self.optimizer_min)
+
+        self.assertEqual(len(self.pop), len(new_pop))
+        self.assertNotEqual(self.pop, new_pop)
+
+
+class TestEpsilonDecayGaussianMutation(unittest.TestCase):
+    """
+    Tests the class EpsilonDecayGaussianMutation.
+    """
+
+    def setUp(self):
+        self.random = np.random.RandomState(0)
+        self.cs = HyperparameterConfigurationSpace(
+            values={
+                "max_episodes": Constant("max_episodes", default=3),
+                "max_episode_length": Constant("max_episode_length", default=1000),
+                "hidden1_shape": Constant("hidden1_shape", default=64),
+                "hidden2_shape": Constant("hidden2_shape", default=32),
+                "fc1.weight": Float("fc1.weight", bounds=(-1.0, 1.0), shape=(64, 8)),
+                "fc1.bias": Float("fc1.bias", bounds=(-1.0, 1.0), shape=(64,)),
+                "fc2.weight": Float("fc2.weight", bounds=(-1.0, 1.0), shape=(32, 64)),
+                "fc2.bias": Float("fc2.bias", bounds=(-1.0, 1.0), shape=(32,)),
+                "fc3.weight": Float("fc3.weight", bounds=(-1.0, 1.0), shape=(4, 32)),
+                "fc3.bias": Float("fc3.bias", bounds=(-1.0, 1.0), shape=(4,)),
+            },
+            seed=0,
+        )
+        self.pop = self.cs.sample_configuration(10)
+        self.fitness = [-92, -17, 22, 56, -96, -20, 76, 29, -48, -56]
+        self.optimizer_min = "min"
+        self.optimizer_max = "max"
+        self.mutator = EpsilonDecayGaussianMutation(mean=0.0, max_std=1.0, min_std=0.1, prob=1.0, steps=9)
+
+    def test_mutate(self):
+        """
+        Tests the method mutate().
+        """
+        new_pop = self.mutator.mutate(self.random, self.cs, self.pop, self.fitness, self.optimizer_min)
         self.assertEqual(len(self.pop), len(new_pop))
         self.assertNotEqual(self.pop, new_pop)
 
